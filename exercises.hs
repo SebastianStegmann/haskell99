8216378bd848575e9224902ce360aa045af3f8ab
Sebastian Stegmann <92033571+SebastianStegmann@users.noreply.github.com>
2025-02-10 18:49:41 +0100

    3

--- exercises.hs
+++ exercises.hs
@@ -9,4 +9,8 @@ beforeLast [x] = error "Only one in list"
 beforeLast [x,y] = x
 beforeLast (x:y:rest) = beforeLast (y:rest)
 
-
+elementAt :: [a] -> Int -> a
+elementAt [] _  = error "Empty list"
+elementAt _ n | n < 0  = error "No number"
+elementAt (x:_) 0 = x
+elementAt (_:xs) n = elementAt xs (n - 1)
