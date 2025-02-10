226e1d309ea07d5e883e5f7cc06b854814c3c8e5
Sebastian Stegmann <92033571+SebastianStegmann@users.noreply.github.com>
2025-02-10 18:27:53 +0100

    2

--- exercises.hs
+++ exercises.hs
@@ -3,3 +3,10 @@ findLast [] = error "No such thing"
 findLast [x] = x
 findLast (_:xs) = findLast xs
 
+beforeLast :: [a] -> a
+beforeLast [] = error "Empty list"
+beforeLast [x] = error "Only one in list"
+beforeLast [x,y] = x
+beforeLast (x:y:rest) = beforeLast (y:rest)
+
+
